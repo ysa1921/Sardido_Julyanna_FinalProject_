24139945d09b0ded15c4cac2f17756d4eec5780d
Sardido, Julyanna Shania BSCPE <sardidoyanna@gmail.com>
2024-12-28 04:33:39 +0800

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JannDamo
+ */
+public class Customer {
+   String cnic, name;
+    double balance;
+    int pin;
+    String accountNo;
+    String card;
+public Customer() {
+}
+public Customer (String cnic, String name, double balance, int pin, String accountNO, String card) {
+    this.cnic = cnic;
+    this.name = name;
+    this.balance = balance;
+    this.pin = pin;
+    this.accountNo = accountNo;
+    this.card = card; 
+} 
+public boolean withdraw (double amount)
+{
+    if (this.balance-amount>=0) 
+    {
+      this.balance-=amount;
+      return true;
+    }
+    return false;
+    
+}
+public void deposit(double amount)
+{
+    this.balance+=amount;
+}
+public String getCnic(){
+    return cnic;
+}
+public String getName(){
+    return name;
+}
+public double getBalance(){
+    return balance;
+}
+public int getPin(){
+    return pin;
+}
+public String getAccountNo(){
+    return accountNo;
+}
+public String getCard(){
+    return card;
+}
+@Override
+public String toString(){
+    return cnic+";"+name+";"+balance+";"+pin+";"+accountNo+";"+card;
+}  
+}
